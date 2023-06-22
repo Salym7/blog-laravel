601c73fec1689b1640949045527767019601f502
Oleksandr Solianyk <salym3k@gmail.com>
2023-06-22 16:01:34 +0300

    add CRUD in categories table

---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('admin.layouts.main')
+
+@section('content')
+     <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Edit categories</h1>
+            <h2 class="m-0">{{$category->title}}</h2>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard v1</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-12">
+
+           <form action="{{route('admin.category.update', $category->id)}}" method="POST" class="w-25">
+            @csrf
+            @method('PATCH')
+            <div class="form-group">
+              <label for="title">Title</label>
+              <input type="text" value="{{$category->title}}" name="title" class="form-control" id="title" placeholder="Category`s name">
+            </div>
+            @error('title')
+              <div class="text-danger mb-2">{{$message}}</div>  
+            @enderror
+            <input type="submit" class="btn btn-primary" value="Update">
+           </form>
+          </div>
+        </div>
+        <!-- /.row -->
+
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,20 @@ Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
     Route::get('/', IndexController::class)->name('home');
 });
 
-Route::group(['namespace' => 'App\Http\Controllers\Admin\Main', 'prefix' => 'admin'], function () {
-    Route::get('/',     IndexController::class)->name('index');
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', IndexController::class)->name('main.admin');
+    });
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
+        Route::get('/', IndexController::class)->name('admin.category.index');
+        Route::get('/create', CreateController::class)->name('admin.category.create');
+        Route::post('/', StoreController::class)->name('admin.category.store');
+        Route::get('/{category}', ShowController::class)->name('admin.category.show');
+        Route::get('/{category}/edit', EditController::class)->name('admin.category.edit');
+        Route::patch('/{category}', UpdateController::class)->name('admin.category.update');
+        Route::delete('/{category}', DeleteController::class)->name('admin.category.delete');
+    });
 });
 
 Auth::routes();
